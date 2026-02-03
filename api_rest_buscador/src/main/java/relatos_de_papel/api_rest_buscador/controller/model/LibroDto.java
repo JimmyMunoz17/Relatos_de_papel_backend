41f214190546f03c15e6840103ba549a208755b1
Mateo <manjarresmateo@gmail.com>
2026-02-03 03:45:29 -0500

    Add stock management methods to LibroDto and remove outdated updateStock method from LibrosServiceImpl

--- api_rest_buscador/src/main/java/relatos_de_papel/api_rest_buscador/controller/model/LibroDto.java
+++ api_rest_buscador/src/main/java/relatos_de_papel/api_rest_buscador/controller/model/LibroDto.java
@@ -85,4 +85,20 @@ public class LibroDto {
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public Integer getStock() {
+        return stock;
+    }
+    
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
 }
